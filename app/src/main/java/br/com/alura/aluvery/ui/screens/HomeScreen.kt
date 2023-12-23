4b89cab323c6a9aa2ed15be032bbc285fe2f08d4
BillVGN <willian.martins.costa@gmail.com>
2023-12-23 12:02:03 -0300

    Parei no OutlinedTextField, remember e mutableStateOf

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,13 +1,18 @@
 package br.com.alura.aluvery.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,26 +21,37 @@ import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.components.ProductsSection
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
+const val TAG = "HomeScreen"
+
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    Column(
-        Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Spacer(Modifier)
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            ProductsSection(
-                title = title,
-                products = products
-            )
+    Column {
+        var text by remember {
+            mutableStateOf("")
         }
-        Spacer(Modifier)
+        OutlinedTextField(value = text, onValueChange = {newValue ->
+            text = newValue
+        })
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(all = 16.dp)
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
+                        title = title,
+                        products = products
+                    )
+                }
+
+            }
+        }
     }
 }
 
